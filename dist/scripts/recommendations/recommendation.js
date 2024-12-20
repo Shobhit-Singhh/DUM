3cdb7d6225401b4c1e37de723d6b1a657b7bbcce
Shobhit Singh <shobhit22iit@gmail.com>
2024-12-20 20:34:18 +0530

    phase 1: deployment 21

--- dist/scripts/recommendations/recommendation.js
+++ dist/scripts/recommendations/recommendation.js
@@ -8,8 +8,8 @@ document.addEventListener("DOMContentLoaded", () => {
                     <!-- Card 1 -->
                     <div class="bg-white shadow-lg rounded-lg overflow-hidden">
                         <div class='flex items-center justify-center mt-6 rounded-md'>
-                            <img class="h-44" src="./assets/Recommendation/r2.avif" alt="">
-                            <img class="h-44" src="./assets/Recommendation/r1.avif" alt="">
+                            <img class="h-44" src="/assets/Recommendation/r2.avif" alt="">
+                            <img class="h-44" src="/assets/Recommendation/r1.avif" alt="">
                         </div>
                         <div class="p-4">
                             <h3 class="text-xl font-bold text-green-800">Personal Recommendation</h3>
@@ -21,7 +21,7 @@ document.addEventListener("DOMContentLoaded", () => {
                     <!-- Card 2 -->
                     <div class="bg-white shadow-lg rounded-lg overflow-hidden">
                         <div class='flex items-center justify-center mt-6 rounded-md '>
-                            <img class="w-2/3 h-44" src="./assets/Recommendation/r5.avif" alt="">
+                            <img class="w-2/3 h-44" src="/assets/Recommendation/r5.avif" alt="">
                             <img class="h-44" src="./assets/Recommendation/r6.avif" alt="">
                         </div>
                         <div class="p-4">
